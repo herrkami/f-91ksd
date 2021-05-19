ab9a6fe33e9d18b3e29435009246010155293a00
Korbinian Schreiber <kosch@kip.uni-heidelberg.de>
2021-05-19 22:24:27 +0200

    Add flashlight service in backlight routine

--- fw/common/svc/backlight.c
+++ fw/common/svc/backlight.c
@@ -3,33 +3,56 @@
 #include "common/hal/hal.h"
 #include "platform.h"
 
-static uint8_t SECTION_INFOMEM g_brightness = 1;
-static uint8_t SECTION_INFOMEM g_timeout = 3;
+static uint8_t SECTION_INFOMEM bl_brightness = 1;
+static uint8_t SECTION_INFOMEM bl_timeout = 3;
+static uint8_t SECTION_INFOMEM fl_brightness = 1;
+static uint8_t SECTION_INFOMEM fl_timeout = 3;
 static uint8_t timer;
+static uint8_t state;
+
+#define STATE_OFF			0
+#define STATE_FLASHLIGHT	(1<<1)
+#define STATE_BACKLIGHT		(1<<2)
+
 
 
 void svc_backlight_brightness_set(uint8_t brightness) {
-	g_brightness = brightness;
+	bl_brightness = brightness;
 }
 uint8_t svc_backlight_brightness_get(void) {
-	return g_brightness;
+	return bl_brightness;
 }
 
 void svc_backlight_timeout_set(uint8_t timeout) {
-	g_timeout = timeout;
+	bl_timeout = timeout;
 }
 
 uint8_t svc_backlight_timeout_get(void) {
-	return g_timeout;
+	return bl_timeout;
+}
+
+void svc_flashlight_brightness_set(uint8_t brightness) {
+	fl_brightness = brightness;
+}
+uint8_t svc_flashlight_brightness_get(void) {
+	return fl_brightness;
+}
+
+void svc_flashlight_timeout_set(uint8_t timeout) {
+	fl_timeout = timeout;
+}
+
+uint8_t svc_flashlight_timeout_get(void) {
+	return fl_timeout;
 }
 
 // void svc_backlight_process(svc_main_proc_event_t ev) {
 // 	if(ev & SVC_MAIN_PROC_EVENT_KEY_UP_LONG) {
-// 		timer = g_timeout*4;
-// 		hal_backlight_set(g_brightness);
+// 		timer = bl_timeout*4;
+// 		hal_backlight_set(bl_brightness);
 // 	}
 // 	if((ev & (SVC_MAIN_PROC_EVENT_KEY_ANY | SVC_MAIN_PROC_EVENT_KEY_ANY_LONG)) && timer) {
-// 		timer = g_timeout*4;
+// 		timer = bl_timeout*4;
 // 	}
 //
 // 	if(ev & SVC_MAIN_PROC_EVENT_TICK) { //decrement
@@ -43,17 +66,43 @@ uint8_t svc_backlight_timeout_get(void) {
 // }
 void svc_backlight_process(svc_main_proc_event_t ev) {
 	if(ev & SVC_MAIN_PROC_EVENT_KEY_UP_LONG) {
-		timer = g_timeout*4;
-		svc_flash_backled_timed(g_timeout*128, g_brightness);
+		if(state == STATE_OFF) {
+			timer = bl_timeout*4;
+			svc_flash_backled_timed(bl_timeout*128, bl_brightness);
+			state = STATE_BACKLIGHT;
+		}
+		else if(state == STATE_BACKLIGHT) {
+			timer = bl_timeout*4;
+			svc_flash_backled_timed(0, bl_brightness);
+			svc_flash_caseled_timed(fl_timeout*128, fl_brightness);
+			state = STATE_FLASHLIGHT;
+		}
+		else if(state == STATE_FLASHLIGHT) {
+			timer = bl_timeout*4;
+			svc_flash_caseled_timed(0, fl_brightness);
+			svc_flash_backled_timed(bl_timeout*128, bl_brightness);
+			state = STATE_BACKLIGHT;
+		}
 	}
 	if((ev & (SVC_MAIN_PROC_EVENT_KEY_ANY | SVC_MAIN_PROC_EVENT_KEY_ANY_LONG)) && timer) {
-		timer = g_timeout*4;
-		svc_flash_backled_timed(g_timeout*128, g_brightness);
+		switch (state) {
+			case STATE_BACKLIGHT:
+				timer = bl_timeout*4;
+				svc_flash_backled_timed(bl_timeout*128, bl_brightness);
+			break;
+			case STATE_FLASHLIGHT:
+				timer = fl_timeout*4;
+				svc_flash_caseled_timed(fl_timeout*128, fl_brightness);
+			break;
+		}
 	}
 
 	if(ev & SVC_MAIN_PROC_EVENT_TICK) { //decrement
 		if(timer) {
 			timer--;
+			if(timer == 0) {
+				state = STATE_OFF;
+			}
 		}
 	}
 }
--- fw/common/svc/backlight.h
+++ fw/common/svc/backlight.h
@@ -8,4 +8,10 @@ uint8_t svc_backlight_brightness_get(void);
 void svc_backlight_timeout_set(uint8_t timeout);
 uint8_t svc_backlight_timeout_get(void);
 
+void svc_flashlight_brightness_set(uint8_t brightness);
+uint8_t svc_flashlight_brightness_get(void);
+
+void svc_flashlight_timeout_set(uint8_t timeout);
+uint8_t svc_flashlight_timeout_get(void);
+
 void svc_backlight_process(svc_main_proc_event_t ev);
